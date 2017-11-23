c1cd3748301dbcdf94886c54d6e95e0ad2eb8063
Hadrien VINAY <hadrien.vinay@yahoo.fr>
2017-11-23 15:06:36 +0100

    grid structure

--- ReversiProject/grille.c
+++ ReversiProject/grille.c
@@ -1,10 +1,41 @@
 #include "header.h"
 
+struct slot *matrice[SIZE][SIZE];
+
+void initslot(){
+    int i,j;
+    struct slot *newslot,*nextslot;
+    newslot = (struct slot *)malloc(sizeof(struct slot));
+    nextslot = (struct slot *)malloc(sizeof(struct slot));
+
+    for(i=0;i<SIZE;i++){
+        for(j=0;j<SIZE;j++){
+            newslot->next=NULL;
+            newslot->x=i;
+            newslot->y=j;
+            newslot->state=0;
+            if((i==SIZE/2-1 && j==SIZE/2-1) || (i==SIZE/2 && j==SIZE/2)){
+                newslot->state=2;
+            }
+            else if((i==SIZE/2 && j==SIZE/2-1) || (i==SIZE/2-1 && j==SIZE/2)) {
+                newslot->state=1;
+            }
+            else{
+                newslot->state=0;
+            }
+            matrice[i][j]=newslot;
+            //printf("%d\n",matrice[i][j]->state);
+            newslot->next = (struct slot *)malloc(sizeof(struct slot));
+            newslot = newslot->next;
+        }
+    }
+
+}
+
 void creationGrille(){
 int i,j;
 char num = '1';
 char car='A';
-char grille[SIZE][SIZE]={[0 ... SIZE-1][0 ... SIZE-1] = '0'};
 
 for(i=0;i<SIZE+1;i++){
     for(j=0;j<SIZE+1;j++){
@@ -20,15 +51,46 @@ for(i=0;i<SIZE+1;i++){
             num++;
         }
         else {
-        printf("N");
+        printf("%d",matrice[i-1][j-1]->state);
         }
         printf(" ");
     }
 printf("\n");
 }
 
-
 }
+
+void updateBlock(int one,int two){
+
+matrice[one-1][two-1]->state=2;
+char num = '1';
+char car='A';
+int i,j;
+
+    for(i=0;i<SIZE+1;i++){
+        for(j=0;j<SIZE+1;j++){
+            if(i==0 && j==0){
+            printf(" ");
+            }
+            else if(i==0){
+            printf("%c",car);
+            car++;
+            }
+            else if(j==0){
+                printf("%c",num);
+                num++;
+            }
+            else {
+            printf("%d",matrice[i-1][j-1]->state);
+            }
+            printf(" ");
+        }
+    printf("\n");
+    }
+}
+
+
+
 
 //update 4block in the grid with one byte
 void updateGrid(int number){
--- ReversiProject/main.c
+++ ReversiProject/main.c
@@ -1,11 +1,19 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
+//#include <ncurses.h>
 #include "header.h"
 
 int main(int argc , char *argv[]) {
 
+int mouv[2];
+int one;
+int two;
+initslot();
 creationGrille();
-updateGrid(0b10010011);
+//updateGrid(0b10010011);
+gets(mouv);
+one=mouv[0];
+two=mouv[1];
+printf("%d,%d",mouv[0],mouv[1]);
+updateBlock(1,1);
+
 return 0;
 }
